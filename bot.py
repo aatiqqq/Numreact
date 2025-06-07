bfeb12658b952bcf2b51756e5e1a332016690805
Rebelhere <muhammadrahimusman@gmail.com>
2025-06-08 00:13:04 +0500

    keep_alive

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+from keep_alive import keep_alive
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -24,4 +25,5 @@ async def on_message(message):
 
 
 load_dotenv()
+keep_alive()
 bot.run(os.getenv('DISCORD_TOKEN'))  # Load token from environment variable
