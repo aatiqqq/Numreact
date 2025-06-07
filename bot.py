7e31c78894b91dca9146efafada343960fe1b088
Rebelhere <muhammadrahimusman@gmail.com>
2025-06-07 23:40:47 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'✅ Logged in as {bot.user}')
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    if message.content.isdigit():
+        await message.add_reaction('🐐')
+
+    await bot.process_commands(message)
+
+
+load_dotenv()
+bot.run(os.getenv('DISCORD_TOKEN'))  # Load token from environment variable
